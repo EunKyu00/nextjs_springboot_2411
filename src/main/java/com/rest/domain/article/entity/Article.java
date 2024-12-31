8ecc9853a937900b0f30d795b7309314fa61aa39
EunKyu00 <ahz1105@naver.com>
2024-12-31 12:27:27 +0900

    article get

--- src/main/java/com/rest/domain/article/entity/Article.java
+++ src/main/java/com/rest/domain/article/entity/Article.java
@@ -1,2 +1,10 @@
-package com.rest.domain.article.entity;public class Article {
+package com.rest.domain.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+    private Long id;
 }
