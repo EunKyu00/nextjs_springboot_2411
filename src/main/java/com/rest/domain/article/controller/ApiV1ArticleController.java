9b95e8c5e016f5c88d9fe21715af1edf3e59df96
EunKyu00 <ahz1105@naver.com>
2024-12-31 12:21:34 +0900

    articles get

---
+++ src/main/java/com/rest/domain/article/controller/ApiV1ArticleController.java
@@ -0,0 +1,14 @@
+package com.rest.domain.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/articles")
+public class ApiV1ArticleController {
+    @GetMapping("")
+    public 
+}
