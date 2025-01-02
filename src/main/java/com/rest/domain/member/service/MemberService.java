5c2765ba804401fb81bad1c15da713be5bf3fbe8
EunKyu00 <ahz1105@naver.com>
2025-01-02 17:48:25 +0900

    make accessToken

--- src/main/java/com/rest/domain/member/service/MemberService.java
+++ src/main/java/com/rest/domain/member/service/MemberService.java
@@ -2,13 +2,15 @@ package com.rest.domain.member.service;
 
 import com.rest.domain.member.entity.Member;
 import com.rest.domain.member.repository.MemberRepository;
+import com.rest.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    public final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final JwtProvider jwtProvider;
 
     public Member join(String username, String password, String email) {
         Member member = Member.builder()
@@ -19,5 +21,14 @@ public class MemberService {
 
         memberRepository.save(member);
         return member;
+    }
+
+    public void authAndMakeTokens(String username, String password) {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
+
+        String accessToken = jwtProvider.genToken(member, 60 * 60 * 5);
+
+        System.out.println("accessToken" + accessToken);
     }
 }
