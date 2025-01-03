e7627818d473ca196bc78b5a32c9753e19324bd6
EunKyu00 <ahz1105@naver.com>
2025-01-03 11:10:49 +0900

    front accessToken cookie

--- src/main/java/com/rest/global/security/ApiSecurityConfig.java
+++ src/main/java/com/rest/global/security/ApiSecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -30,23 +33,28 @@ public class ApiSecurityConfig {
                                 .requestMatchers(HttpMethod.POST,"/api/*/members/login").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .csrf(
-                        csrf -> csrf
-                                .disable()
-                )//csrf 토큰 끄기
-                .httpBasic(
-                        httpBasic -> httpBasic.disable()
-                ) //httpBasic 로그인 방식 끝기
-                .formLogin(
-                        formLogin -> formLogin.disable()
-                )// 폼 로그인 방식 끄기
-                .sessionManagement(
-                        sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS)
-                )//세션 끄기
+                .csrf(csrf -> csrf.disable())//csrf 토큰 끄기
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // cors 설정 추가
+                .httpBasic(httpBasic -> httpBasic.disable()) //httpBasic 로그인 방식 끝기
+                .formLogin(formLogin -> formLogin.disable())// 폼 로그인 방식 끄기
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS))//세션 끄기
                 .addFilterBefore(
                         jwtAuthorizationFilter, //엑세스 토큰을 이용한 로그인 처리
                         UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000"); // 허용할 출처 추가
+        corsConfiguration.addAllowedOrigin("http://cdpn.io"); //추가 허용 출처
+        corsConfiguration.addAllowedMethod("*"); //모든 HTTP 메서드 허용
+        corsConfiguration.addAllowedHeader("*"); // 모든 요청 헤더 허용
+        corsConfiguration.setAllowCredentials(true); //쿠키 및 인증 정보 포함 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration); //모든 경로에 CORS 정책 적용
+        return source;
+    }
+
 }
